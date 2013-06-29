cc0949e9893fdc11f632e95994dbcfa7318df99b
Camilo Palma Pradena <cpalmapradena@gmail.com>
2013-06-29 11:23:39 -0400

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrapatonTwitter::Application.config.secret_token = '09cf1ebc732aa86d59bf2b64b7d286833adc72c00933f106823e6da3917211595ee320e5cab36ecfb2f0758bf2b751d688dbb2be1aeabb55573a20aedf1b7264'
