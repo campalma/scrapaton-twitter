5b1401037666f7d527479eae9909a5afd45d405a
Camilo Palma Pradena <cpalmapradena@gmail.com>
2013-06-29 16:32:13 -0400

    Group by

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,20 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.where(:candidate.ne => nil).sort(:created_at).all
+    @tweets = Tweet.where(:candidate.ne => nil).sort(:created_at).limit(100).all
+    @groups = {}
+
+    @tweets.each do |t|
+        if @groups[t.created_at.min].nil?
+            @groups[t.created_at.min] = []
+        else
+            @groups[t.created_at.min] << t
+        end
+    end
+
     @min = @tweets.first.created_at.to_i
     @max = @tweets.last.created_at.to_i
-    puts @min
-    puts @max
     @diff= @max - @min
     @a = 700.0/@diff
     @b = -700*@min/(@diff)
-    puts @a
-    puts @b
   end
 end
